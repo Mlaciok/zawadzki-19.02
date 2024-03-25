67b39aef7535441b8f0267ddaeffade58db1e287
Mlaciok <rozalewski.gd@gmail.com>
2024-03-25 13:01:09 +0100

    cos

---
+++ upload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dodaj nowy post</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+     <label for="postTitleInput">tutył posta</label>
+        <input type="text" name="postTitle">
+        <br>
+        <input type="text" name="postDescription">
+    
+    </form>
+</body>
+</html>
